7bc989da5be840a2669758c01d585e4d9017fcb9
rlevidev <rlevi.dev@gmail.com>
2026-02-09 17:46:24 -0300

    feat(pix-key-service): Adiciona funcionalidades de busca e exclusão de chaves Pix
    Implementa novos métodos no PixKeyService para:
    - Buscar uma chave Pix pelo seu valor.
    - Buscar todas as chaves Pix associadas a uma conta.
    - Excluir uma chave Pix pelo seu valor.
    
    Utiliza PixKeyMapper para converter entidades PixKey em PixKeyResponseDTOs.
    Introduz PixKeyNotFoundException para tratar casos de chaves não
    encontradas durante as operações de busca e exclusão.
    ```

--- pix-key-service/src/main/java/com/banking/pixkey/domain/service/PixKeyService.java
+++ pix-key-service/src/main/java/com/banking/pixkey/domain/service/PixKeyService.java
@@ -1,8 +1,11 @@
 package com.banking.pixkey.domain.service;
 
 import com.banking.pixkey.api.dto.PixKeyRequestDTO;
+import com.banking.pixkey.api.dto.PixKeyResponseDTO;
+import com.banking.pixkey.api.mapper.PixKeyMapper;
 import com.banking.pixkey.domain.exception.InvalidAccountException;
 import com.banking.pixkey.domain.exception.PixKeyAlreadyExistsException;
+import com.banking.pixkey.domain.exception.PixKeyNotFoundException;
 import com.banking.pixkey.domain.model.PixKey;
 import com.banking.pixkey.domain.repository.PixKeyRepository;
 import com.banking.pixkey.infrastructure.client.AccountServiceClient;
@@ -10,11 +13,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 public class PixKeyService {
   private final PixKeyRepository pixKeyRepository;
   private final AccountServiceClient accountServiceClient;
+  private final PixKeyMapper pixKeyMapper;
 
   @Transactional
   public PixKey registerKey(PixKeyRequestDTO dto) {
@@ -34,5 +41,25 @@ public class PixKeyService {
     pixKey.setAccountId(dto.accountId());
 
     return pixKeyRepository.save(pixKey);
+  }
+
+  @Transactional(readOnly = true)
+  public PixKeyResponseDTO findByKeyValue(String keyValue) {
+    return pixKeyRepository.findByKeyValue(keyValue)
+            .map(pixKeyMapper:: toResponseDTO)
+            .orElseThrow(() -> new PixKeyNotFoundException(keyValue));
+  }
+
+  @Transactional(readOnly = true)
+  public List<PixKeyResponseDTO> findByAccountId(UUID accountId) {
+    List<PixKey> keys = pixKeyRepository.findByAccountId(accountId);
+    return pixKeyMapper.toResponseList(keys);
+  }
+
+  public void deleteKey(String keyValue) {
+    PixKey pixKey = pixKeyRepository.findByKeyValue(keyValue)
+            .orElseThrow(() -> new PixKeyNotFoundException(keyValue));
+
+    pixKeyRepository.delete(pixKey);
   }
 }
