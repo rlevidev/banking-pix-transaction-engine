cdd8086f64524bbe19fba0daae38c1210ca0077a
rlevidev <rlevi.dev@gmail.com>
2026-02-13 17:12:57 -0300

    refactor(transaction-service): Refatorar PixKeyServiceClient
    Remover construtor manual para injecao de dependencia.
    Adicionar @RequiredArgsConstructor do Lombok para geracao do construtor.
    Adicionar @Slf4j para suporte a logs na classe.
    Marcar a classe como um componente Spring com @Component.

--- transaction-service/src/main/java/com/banking/transactionservice/infrastructure/client/PixKeyServiceClient.java
+++ transaction-service/src/main/java/com/banking/transactionservice/infrastructure/client/PixKeyServiceClient.java
@@ -1,15 +1,17 @@
 package com.banking.transactionservice.infrastructure.client;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class PixKeyServiceClient {
   private final RestTemplate restTemplate;
 
   @Value("${services.pix-key.url:http://localhost:8082}")
-
   private String pixKeyServiceUrl;
-  public PixKeyServiceClient(RestTemplate restTemplate) {
-    this.restTemplate = restTemplate;
-  }
 }
