04ee3a3d57717ceeaf096f19a2c13ef5ce2979a3
rlevidev <rlevi.dev@gmail.com>
2026-02-16 17:24:20 -0300

    feat(transaction-service): Adiciona getters para IDs e chaves Pix em exceções
    Permite que o ID da transação e a chave Pix sejam acessados
    diretamente das exceções `TransactionNotFoundException` e
    `PixKeyNotFoundException`, facilitando o tratamento de erros.

--- transaction-service/src/main/java/com/banking/transactionservice/domain/exception/PixKeyNotFoundException.java
+++ transaction-service/src/main/java/com/banking/transactionservice/domain/exception/PixKeyNotFoundException.java
@@ -1,7 +1,14 @@
 package com.banking.transactionservice.domain.exception;
 
 public class PixKeyNotFoundException extends RuntimeException {
+  private final String pixKey;
+
   public PixKeyNotFoundException(String pixKey) {
     super(String.format("Pix Key with value %s not found", pixKey));
+    this.pixKey = pixKey;
+  }
+
+  public String getPixKey() {
+    return pixKey;
   }
 }
--- transaction-service/src/main/java/com/banking/transactionservice/domain/exception/TransactionNotFoundException.java
+++ transaction-service/src/main/java/com/banking/transactionservice/domain/exception/TransactionNotFoundException.java
@@ -3,7 +3,13 @@ package com.banking.transactionservice.domain.exception;
 import java.util.UUID;
 
 public class TransactionNotFoundException extends RuntimeException {
+  private final UUID transactionId;
   public TransactionNotFoundException(UUID transactionId) {
     super(String.format("Transaction with ID %s not found", transactionId));
+    this.transactionId = transactionId;
+  }
+
+  public UUID getTransactionId() {
+    return transactionId;
   }
 }
