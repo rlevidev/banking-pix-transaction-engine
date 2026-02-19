0f5270342d6220325e6db5aefec18266e25f5eec
rlevidev <rlevi.dev@gmail.com>
2026-02-19 16:55:18 -0300

    feat(auth-service): Adiciona busca e verificação de usuário por nome de usuário
    Introduz os métodos `existsByUsername` e `findByUsername`
    no `UserRepository`. Isso permite verificar a existência
    e recuperar usuários usando o nome de usuário.

--- auth-service/src/main/java/com/banking/authservice/domain/repository/UserRepository.java
+++ auth-service/src/main/java/com/banking/authservice/domain/repository/UserRepository.java
@@ -8,5 +8,7 @@ import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
   boolean existsByEmail(String email);
+  boolean existsByUsername(String username);
   Optional<User> findByEmail(String email);
+  Optional<User> findByUsername(String username);
 }
